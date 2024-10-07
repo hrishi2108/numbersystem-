d2ce6aa66aa993bea30f0d77a8baa08463e4eec2
Hrishi <rishiyadav2108@gmail.com>
2024-10-07 13:33:13 +0530

    even and odd

---
+++ index.js
@@ -0,0 +1,12 @@
+let numbers = [3,7,2,15,9,20,5,14,12,1,8,11,6,19,4,10,17,13,16,18]
+for(let i=0;i<numbers.length;i++)
+{
+    if(i % 2 == 0)
+    {
+        comsole.log(i , "it is Even Number ")
+    }
+    else if(i % 2 == 1)
+    {
+        console.log(i , "Its is Odd Number ")
+    }
+}
